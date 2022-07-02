0d1f2bba8e0a71c7cf39300157b11bc395e505a7
aleksei@mazur <mazuralexey@gmail.com>
2022-07-02 19:29:01 +0300

    pages

--- 1. API/github_API.py
+++ 1. API/github_API.py
@@ -1,9 +1,6 @@
 """
 Быстрый старт с GitHub API
-Показывает список всех репозиториев ползователя
-* Показывает список только с первой страницы (т.е ограничение 30)
-
-* curl https://api.github.com/users/miguelgrinberg/repos > curl_repos_list.json
+Показывает список всех репозиториев ползователя, сохраняет полный ответ в файл
 
 """
 
@@ -11,29 +8,31 @@ import requests
 import json
 
 params = {'login': 'miguelgrinberg',
-          'private': False}
+          'private': False,
+          'per_page': 50,
+          'page': 1}
 
-url_repos = f"https://api.github.com/users/{params['login']}/repos"
-url_user = f"https://api.github.com/users/{params['login']}"
-
-response_1 = requests.get(url=url_user, params=params)
-response_2 = requests.get(url=url_repos, params=params)
-
-j_data_1 = response_1.json()
-j_data_2 = response_2.json()
+url_user = f"https://api.github.com/users/{params['login']}"  # пользователь и информ. о нем
+url_repos = f"https://api.github.com/users/{params['login']}/repos"  # все репозитории пользователя и информ. о них
 
 
 def show_repos_list():
-    print(f"Total public repositories for this user is {j_data_1['public_repos']}")
-    for repo in j_data_2:
-        print(repo['html_url'])
+    response_1 = requests.get(url=url_user, params=params)
+    j_data_user = response_1.json()
+    print(f"Total public repositories for this user is {j_data_user['public_repos']}")
+    while True:
+        response_2 = requests.get(url=url_repos, params=params)
+        j_data_repos = response_2.json()
+        if len(j_data_repos) == 0:
+            break
+        for repo in j_data_repos:
+            print(repo.get('html_url'))
+            params['page'] += 1
+            print(params['page'])
 
-
-def save_outfile():
-    with open('json_data.json', 'w') as outfile:
-        json.dump(j_data_2, outfile, indent=2)
+            with open('json_data.json', 'a+') as outfile:
+                json.dump(j_data_repos, outfile, indent=2)
 
 
 if __name__ == "__main__":
     show_repos_list()
-    save_outfile()
